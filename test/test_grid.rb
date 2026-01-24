91118162d15c713d1972d145f31f370be0785b05
James Cook <jcook.rubyist@gmail.com>
2026-01-24 05:22:01 -0600

    Consolidate tests for lib/tk.rb

--- test/test_grid.rb
+++ test/test_grid.rb
@@ -1,46 +1,13 @@
 # frozen_string_literal: true
 
-# Test for Tk.grid, Tk.grid_forget, Tk.ungrid and TkGrid module
+# Tests for lib/tk/grid.rb (TkGrid module)
+# Note: Tk.grid, Tk.grid_forget, Tk.ungrid are tested in test_tk_module.rb
 
 require_relative 'test_helper'
 require_relative 'tk_test_helper'
 
 class TestGrid < Minitest::Test
   include TkTestHelper
-
-  def test_grid_methods
-    assert_tk_app("Tk.grid methods", method(:grid_app))
-  end
-
-  def grid_app
-    require 'tk'
-
-    errors = []
-
-    # Create widgets
-    lbl = TkLabel.new(root, text: "Test")
-    btn = TkButton.new(root, text: "Button")
-
-    # Tk.grid - arrange in grid
-    Tk.grid(lbl, row: 0, column: 0)
-    Tk.grid(btn, row: 1, column: 0)
-
-    # Verify they're gridded
-    errors << "label not gridded" unless lbl.winfo_manager == "grid"
-    errors << "button not gridded" unless btn.winfo_manager == "grid"
-
-    # Tk.grid_forget - remove from grid
-    Tk.grid_forget(lbl)
-    errors << "label should be ungridded" unless lbl.winfo_manager == ""
-
-    # Tk.ungrid - alias for grid_forget
-    Tk.ungrid(btn)
-    errors << "button should be ungridded" unless btn.winfo_manager == ""
-
-    raise "Grid test failures:\n  " + errors.join("\n  ") unless errors.empty?
-  end
-
-  # --- TkGrid module tests ---
 
   def test_tkgrid_configure
     assert_tk_app("TkGrid.configure", method(:app_tkgrid_configure))
