787d3e7f9023d74bbd605cb77247bd2945da861d
James Cook <jcook.rubyist@gmail.com>
2026-02-03 05:40:44 -0600

    Panedwindow

--- lib/tk/panedwindow.rb
+++ lib/tk/panedwindow.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: false
-require 'tk/option_dsl'
+require_relative 'core/callable'
+require_relative 'core/configurable'
+require_relative 'callback'
+require_relative 'core/widget'
 
 # A container with resizable panes separated by draggable sashes.
 #
@@ -21,74 +24,40 @@ require 'tk/option_dsl'
 #   paned.add(bottom, minsize: 50)
 #   paned.pack(fill: :both, expand: true)
 #
-# @note Add panes only after their geometry is finalized. Panes don't
-#   auto-resize once mapped.
-#
-# @note Extra space from window resizing goes to the last (rightmost/bottom) pane.
-#
 # @see https://www.tcl-lang.org/man/tcl/TkCmd/panedwindow.html Tcl/Tk panedwindow manual
 #
-class Tk::PanedWindow<TkWindow
+class Tk::PanedWindow
+  include Tk::Core::Callable
+  include Tk::Core::Configurable
+  include TkCallback
+  include Tk::Core::Widget
   include Tk::Generated::Panedwindow
-  # @generated:options:start
-  # Available options (auto-generated from Tk introspection):
-  #
-  #   :background
-  #   :borderwidth
-  #   :cursor
-  #   :handlepad
-  #   :handlesize
-  #   :height
-  #   :opaqueresize
-  #   :orient
-  #   :proxybackground
-  #   :proxyborderwidth
-  #   :proxyrelief
-  #   :relief
-  #   :sashcursor
-  #   :sashpad
-  #   :sashrelief
-  #   :sashwidth
-  #   :showhandle
-  #   :width
-  # @generated:options:end
-
 
   TkCommandNames = ['panedwindow'.freeze].freeze
   WidgetClassName = 'Panedwindow'.freeze
-  WidgetClassNames[WidgetClassName] ||= self
-
-  #def create_self(keys)
-  #  if keys and keys != None
-  #    tk_call_without_enc('panedwindow', @path, *hash_kv(keys, true))
-  #  else
-  #    tk_call_without_enc('panedwindow', @path)
-  #  end
-  #end
-  #private :create_self
+  Tk::Core::Widget.registry[WidgetClassName] ||= self
 
   def add(*args)
     keys = args.pop
     fail ArgumentError, "no window in arguments" unless keys
-    if keys && keys.kind_of?(Hash)
-      fail ArgumentError, "no window in arguments" if args == []
-      # args = args.collect{|w| (w.kind_of?(TkObject))? w.epath: w }
-      args = args.collect{|w| _epath(w) }
-      #args.push(hash_kv(keys))
-      args.concat(hash_kv(keys))
+    if keys.kind_of?(Hash)
+      fail ArgumentError, "no window in arguments" if args.empty?
+      cmd_args = args.map { |w| _pw_path(w) }
+      keys.each do |k, v|
+        cmd_args << "-#{k}"
+        cmd_args << _pw_path(v)
+      end
     else
-      args.push(keys) if keys
-      # args = args.collect{|w| (w.kind_of?(TkObject))? w.epath: w }
-      args = args.collect{|w| _epath(w) }
+      args.push(keys)
+      cmd_args = args.map { |w| _pw_path(w) }
     end
-    tk_send_without_enc('add', *args)
+    tk_send('add', *cmd_args)
     self
   end
 
   def forget(win, *wins)
     wins.unshift(win)
-    # tk_send_without_enc('forget', *((w.kind_of?(TkObject))? w.epath: w))
-    tk_send_without_enc('forget', *(wins.collect{|w| _epath(w)}))
+    tk_send('forget', *(wins.map { |w| _pw_path(w) }))
     self
   end
   alias del forget
@@ -96,146 +65,136 @@ class Tk::PanedWindow<TkWindow
   alias remove forget
 
   def identify(x, y)
-    list(tk_send_without_enc('identify', x, y))
+    result = tk_send('identify', x, y)
+    TclTkLib._split_tklist(result).map { |v| v =~ /\A-?\d+\z/ ? v.to_i : v }
   end
 
   def proxy_coord
-    list(tk_send_without_enc('proxy', 'coord'))
+    result = tk_send('proxy', 'coord')
+    TclTkLib._split_tklist(result).map(&:to_i)
   end
+
   def proxy_forget
-    tk_send_without_enc('proxy', 'forget')
+    tk_send('proxy', 'forget')
     self
   end
+
   def proxy_place(x, y)
-    tk_send_without_enc('proxy', 'place', x, y)
+    tk_send('proxy', 'place', x, y)
     self
   end
 
   def sash_coord(index)
-    list(tk_send('sash', 'coord', index))
+    result = tk_send('sash', 'coord', index)
+    result.split.map(&:to_i)
   end
+
   def sash_dragto(index, x, y)
     tk_send('sash', 'dragto', index, x, y)
     self
   end
+
   def sash_mark(index, x, y)
     tk_send('sash', 'mark', index, x, y)
     self
   end
+
   def sash_place(index, x, y)
     tk_send('sash', 'place', index, x, y)
     self
   end
 
   def panecget_strict(win, key)
-    # win = win.epath if win.kind_of?(TkObject)
-    win = _epath(win)
-    tk_tcl2ruby(tk_send_without_enc('panecget', win, "-#{key}"))
+    val = tk_send('panecget', _pw_path(win), "-#{key}")
+    _tcl_to_ruby(val)
   end
+
   def panecget(win, key)
     panecget_strict(win, key)
   end
 
   def paneconfigure(win, key, value=nil)
-    # win = win.epath if win.kind_of?(TkObject)
-    win = _epath(win)
-    if key.kind_of? Hash
+    w = _pw_path(win)
+    if key.kind_of?(Hash)
       params = []
-      key.each{|k, v|
-        params.push("-#{k}")
-        # params.push((v.kind_of?(TkObject))? v.epath: v)
-        params.push(_epath(v))
-      }
-      tk_send_without_enc('paneconfigure', win, *params)
+      key.each do |k, v|
+        params << "-#{k}"
+        params << _pw_path(v)
+      end
+      tk_send('paneconfigure', w, *params)
     else
-      # value = value.epath if value.kind_of?(TkObject)
-      value = _epath(value)
-      tk_send_without_enc('paneconfigure', win, "-#{key}", value)
+      tk_send('paneconfigure', w, "-#{key}", _pw_path(value))
     end
     self
   end
   alias pane_config paneconfigure
 
   def paneconfiginfo(win, key=nil)
-    if true # FIXME: Forced true after GET_CONFIGINFO_AS_ARRAY removal - needs cleanup
-      # win = win.epath if win.kind_of?(TkObject)
-      win = _epath(win)
-      if key
-        #conf = tk_split_list(tk_send_without_enc('paneconfigure',
-        #                                         win, "-#{key}"))
-        conf = tk_split_list(tk_send_without_enc('paneconfigure',
-                                                 win, "-#{key}"),
-                             0, false, true)
+    w = _pw_path(win)
+    if key
+      conf = TclTkLib._split_tklist(tk_send('paneconfigure', w, "-#{key}"))
+      conf[0] = conf[0][1..-1]
+      conf[3] = _tcl_to_ruby(conf[3]) if conf[3]
+      conf[4] = _tcl_to_ruby(conf[4]) if conf[4]
+      conf
+    else
+      TclTkLib._split_tklist(tk_send('paneconfigure', w)).map do |conflist|
+        conf = TclTkLib._split_tklist(conflist)
         conf[0] = conf[0][1..-1]
-        if conf[0] == 'hide'
-          conf[3] = bool(conf[3]) unless conf[3].empty?
-          conf[4] = bool(conf[4]) unless conf[4].empty?
-        end
+        conf[1] = conf[1][1..-1] if conf.size == 2 # alias info
+        conf[3] = _tcl_to_ruby(conf[3]) if conf[3]
+        conf[4] = _tcl_to_ruby(conf[4]) if conf[4]
         conf
-      else
-        #tk_split_simplelist(tk_send_without_enc('paneconfigure',
-        #                                        win)).collect{|conflist|
-        #  conf = tk_split_simplelist(conflist)
-        tk_split_simplelist(tk_send_without_enc('paneconfigure', win),
-                            false, false).collect{|conflist|
-          conf = tk_split_simplelist(conflist, false, true)
-          conf[0] = conf[0][1..-1]
-          if conf[3]
-            if conf[0] == 'hide'
-              conf[3] = bool(conf[3]) unless conf[3].empty?
-            elsif conf[3].index('{')
-              conf[3] = tk_split_list(conf[3])
-            else
-              conf[3] = tk_tcl2ruby(conf[3])
-            end
-          end
-          if conf[4]
-            if conf[0] == 'hide'
-              conf[4] = bool(conf[4]) unless conf[4].empty?
-            elsif conf[4].index('{')
-              conf[4] = tk_split_list(conf[4])
-            else
-              conf[4] = tk_tcl2ruby(conf[4])
-            end
-          end
-          conf[1] = conf[1][1..-1] if conf.size == 2 # alias info
-          conf
-        }
       end
-    else # ! true
-      # DEAD
     end
   end
   alias pane_configinfo paneconfiginfo
 
   def current_paneconfiginfo(win, key=nil)
-    if true # FIXME: Forced true after GET_CONFIGINFO_AS_ARRAY removal - needs cleanup
-      if key
-        conf = paneconfiginfo(win, key)
-        {conf[0] => conf[4]}
-      else
-        ret = {}
-        paneconfiginfo(win).each{|conf|
-          ret[conf[0]] = conf[4] if conf.size > 2
-        }
-        ret
+    if key
+      conf = paneconfiginfo(win, key)
+      {conf[0] => conf[4]}
+    else
+      ret = {}
+      paneconfiginfo(win).each do |conf|
+        ret[conf[0]] = conf[4] if conf.size > 2
       end
-    else # ! true
-      # DEAD
+      ret
     end
   end
-
   alias current_pane_configinfo current_paneconfiginfo
 
   def panes
-    list(tk_send_without_enc('panes'))
+    result = tk_send('panes')
+    TclTkLib._split_tklist(result)
+  end
+
+  private
+
+  # Convert widget or value to path string for pane commands
+  def _pw_path(v)
+    if v.respond_to?(:epath)
+      v.epath
+    elsif v.respond_to?(:path)
+      v.path
+    else
+      v.to_s
+    end
+  end
+
+  # Simple Tcl→Ruby value conversion
+  def _tcl_to_ruby(val)
+    return val unless val.is_a?(String)
+    case val
+    when '1', 'true', 'yes' then true
+    when '0', 'false', 'no' then false
+    when /\A-?\d+\z/ then val.to_i
+    when '' then val
+    else val
+    end
   end
 end
 
 Tk::Panedwindow = Tk::PanedWindow
-#TkPanedWindow = Tk::PanedWindow unless Object.const_defined? :TkPanedWindow
-#TkPanedwindow = Tk::Panedwindow unless Object.const_defined? :TkPanedwindow
-#Tk.__set_toplevel_aliases__(:Tk, Tk::PanedWindow,
-#                            :TkPanedWindow, :TkPanedwindow)
 Tk.__set_loaded_toplevel_aliases__('tk/panedwindow.rb', :Tk, Tk::PanedWindow,
                                    :TkPanedWindow, :TkPanedwindow)
